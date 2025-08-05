d7fcacf6df4684d3faaf8a3ed0169c7a068dcee3
Parveengill02 <parveenkaurgill02@gmail.com>
2025-08-05 10:04:32 +0530

    update

--- src/api/fakeapi.js
+++ src/api/fakeapi.js
@@ -3,7 +3,7 @@ export const getData = () =>
   new Promise((resolve) => {
     setTimeout(() => {
       const User=['Jon', 'Dev', 'Ram'];
-      const randomFruits = User.sort(() => 0.5 - Math.random()).slice(0, 3);
-      resolve(randomFruits);
+      const randomUser = User.sort(() => 0.5 - Math.random()).slice(0, 3);
+      resolve(randomUser);
     }, 1000);
   });
